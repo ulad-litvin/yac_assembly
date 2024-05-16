de88dc3ba470f5a938741e80ebdbfc5af04f4562
Ulad Litvin <124574161+ulad-litvin@users.noreply.github.com>
2024-05-16 14:19:52 +0100

    Update ref_assembly.py

--- ref_assembly.py
+++ ref_assembly.py
@@ -39,7 +39,6 @@ try:
         sys.exit(1)
     
     ref_seq_path = os.path.join(ref_seq_folder, ref_seq_file[0])
-    print(ref_seq_path)
 
 except IndexError:
     print("Please provide the folder with your reference sequence.")
@@ -55,24 +54,18 @@ Assigning the files with raw reads
 try:
     samples_folder = sys.argv[2]
     samples_files = [f for f in os.listdir(samples_folder) if f.endswith('.fastq')]
-    print(samples_files)
     
     if len(samples_files) == 0:
         print("No FASTQ files found in the raw reads folder.")
         sys.exit(1)
     
     samples_path = [os.path.join(samples_folder, sample) for sample in samples_files]
-    print(samples_path)
 
     samples_path_no_ext = [sample.rstrip('.fastq') for sample in samples_path]
-    print(samples_path_no_ext)
 
     sample_names = [sample.split('/')[-1] for sample in samples_path_no_ext]
-    print(sample_names)
 
-    # make a dictionary with sample names as keys and sample paths as values
     samples_dict = dict(zip(sample_names, samples_path_no_ext))
-    print(samples_dict)
 
 except IndexError:
     print("Please provide the path to a folder with your raw reads.")
